9af4395c1dd3dce14ec659deea88e1d2f3483de7
Lincoln Bergeson <lincoln.bergeson@gmail.com>
2023-11-30 19:57:45 -0600

    refactoring and formatting

--- examples/stock-chart/index.js
+++ examples/stock-chart/index.js
@@ -1,4 +1,4 @@
-// User config
+// User config (will be properly parameterized in the future)
 
 const options = {
   colors: {
@@ -14,7 +14,7 @@ const chartContainer = "chart-container";
 
 // Library code
 
-// First create the HTML elements we will need
+// First we manually create the HTML elements we will need
 
 const container = document.getElementById(chartContainer);
 container.style.display = "flex";
@@ -37,6 +37,7 @@ infoBoxElem.style.padding = "0.7em";
 infoBoxElem.style.backgroundColor = "#eee";
 infoBoxElem.style.userSelect = "none";
 infoBoxElem.style.width = "20em";
+infoBoxElem.style.fontSize = "14px";
 container.appendChild(infoBoxElem);
 
 const ohlcBoxElem = document.createElement("div");
@@ -86,7 +87,7 @@ volumeBoxElem.appendChild(volumeLabel);
 volumeBoxElem.appendChild(ohlcvElements.volume);
 infoBoxElem.appendChild(volumeBoxElem);
 
-// Define stateful objects
+// Define stateful objects and their member functions
 
 const state = {
   currentBar: null,
@@ -105,12 +106,6 @@ function toggleVolumeVisible() {
 
 volumeBoxElem.addEventListener("click", toggleVolumeVisible);
 
-// Then
-
-function round2(num) {
-  return Math.round(num * 100) / 100;
-}
-
 function updateCurrentPrices(bar) {
   state.currentBar = bar;
   ohlcvElements.open.innerHTML = round2(bar.open);
@@ -122,14 +117,15 @@ function updateCurrentPrices(bar) {
 
 updateCurrentPrices(data[data.length - 1]);
 
-const mousePos = {x: -1, y: -1}
+const mousePos = { x: -1, y: -1 }
 
-function paddedAccessors() {
-  return [d => d.high + (d.high - d.low) / 3, d => d.low - (d.high - d.low) / 3];
+// Quick helper function
+
+function round2(num) {
+  return Math.round(num * 100) / 100;
 }
 
-const xScale = d3.scaleTime().domain(fc.extentDate().accessors([d => d.date])(data));
-const yScale = d3.scaleLinear().domain(fc.extentLinear().accessors(paddedAccessors())(data));
+// Now for some style helper functions
 
 const setFillColor = (opacity) => {
   return (program, data) => {
@@ -145,9 +141,46 @@ const setFillColor = (opacity) => {
   };
 };
 
-const candlestick = fc.autoBandwidth(fc.seriesWebglCandlestick())
-  .decorate(setFillColor(1))
-const lowLine = fc.seriesSvgLine();
+// Here is some hacky CSS to make the OHLC and Volume chart stick so closely to each other
+function specialgrid(sel) {
+  return sel.each(function () {
+    const self = d3.select(this);
+    self.style('display', 'grid');
+    self.style('display', '-ms-grid');
+    self.style('grid-template-columns', 'minmax(0em,max-content) 0fr 1fr 0fr minmax(0em,max-content)');
+    self.style('-ms-grid-columns', 'minmax(0em,max-content) 0fr 1fr 0fr minmax(0em,max-content)');
+    self.style('grid-template-rows', 'minmax(0em,max-content) 0fr 1fr 0fr minmax(0em,max-content)');
+    self.style('-ms-grid-rows', 'minmax(0em,max-content) 0fr 1fr 0fr minmax(0em,max-content)');
+  });
+}
+
+function flatgrid(sel) {
+  return sel.each(function () {
+    const self = d3.select(this);
+    self.style('grid-template-rows', 'minmax(0em, max-content) 0fr 0fr 0fr minmax(0em, max-content)');
+    self.style('-ms-grid-rows', 'minmax(0em, 0em) 0fr 0fr 0fr minmax(0em, 0em)');
+  });
+}
+
+function attr(k, v) {
+  return function (sel) {
+    return sel.each(function () {
+      const self = d3.select(this);
+      self.style(k, v);
+    });
+  };
+}
+
+const displayNone = attr('display', 'none');
+
+// Set up the zooming and scaling
+
+function paddedAccessors() {
+  return [d => d.high + (d.high - d.low) / 3, d => d.low - (d.high - d.low) / 3];
+}
+
+const xScale = d3.scaleTime().domain(fc.extentDate().accessors([d => d.date])(data));
+const yScale = d3.scaleLinear().domain(fc.extentLinear().accessors(paddedAccessors())(data));
 
 const zoom = fc
   .zoom()
@@ -162,6 +195,12 @@ const zoom = fc
     render();
   });
 
+// Define the base charts
+
+const candlestick = fc.autoBandwidth(fc.seriesWebglCandlestick())
+  .decorate(setFillColor(1))
+const lowLine = fc.seriesSvgLine();
+
 const volumeValues = data.map(d => d.volume);
 const maxVolume = Math.max(...volumeValues);
 const minVolume = Math.min(...volumeValues);
@@ -172,10 +211,58 @@ const volume = fc.autoBandwidth(fc.seriesWebglBar())
   .mainValue(d => d.volume)
   .decorate(setFillColor(0.8))
 
+const ohlcChart = fc
+  .chartCartesian(xScale, yScale)
+  .webglPlotArea(candlestick)
+  .svgPlotArea(lowLine)
+  .decorate(sel => {
+    sel.enter().call(specialgrid);
+    sel.enter()
+      .select('.svg-plot-area')
+      .call(attr('border-bottom', '1px solid rgba(0, 0, 0, 0.1)'));
+    sel.enter()
+      .selectAll('.plot-area')
+      .call(zoom, xScale, yScale)
+    sel.enter()
+      .selectAll('.x-axis')
+      .call(displayNone, zoom, xScale);
+    sel.enter()
+      .selectAll('.top-label')
+      .call(displayNone);
+    sel.enter()
+      .selectAll('svg')
+      .call(attr("font-size", "14px"));
+  });
+
+const volumeChart = fc
+  .chartCartesian(xScale, volumeScale)
+  .webglPlotArea(volume)
+  .svgPlotArea(lowLine)
+  .decorate(sel => {
+    sel.enter().call(specialgrid);
+    sel.enter()
+      .selectAll('.plot-area')
+      .call(zoom, xScale, yScale)
+    sel.enter()
+      .selectAll('.x-axis')
+      .call(zoom, xScale);
+    sel.enter()
+      .selectAll('.top-label')
+      .call(displayNone);
+    sel.enter()
+      .selectAll('.right-axis svg')
+      .call(displayNone);
+    sel.enter()
+      .selectAll('svg')
+      .call(attr("font-size", "14px"));
+  });
+
+// The next chunk of code deals with getting the crosshair to work exactly the way I want
+
 function annotationLine(sel) {
   sel.enter()
     .selectAll('g.annotation-line > line')
-    .each(function() {
+    .each(function () {
       const self = d3.select(this);
       self.style('stroke-opacity', '.8');
       self.style('stroke-dasharray', '4');
@@ -202,328 +289,6 @@ const crosshairVertical = fc.annotationSvgCrosshair()
     sel.call(annotationLine);
   });
 
-function show(sel) {
-  return sel.each(function() {
-    const self = d3.select(this);
-    self.style('display', 'block');
-  });
-};
-
-function hide(sel) {
-  return sel.each(function() {
-    const self = d3.select(this);
-    self.style('display', 'none');
-  });
-};
-
-function nogrid(sel) {
-  return sel.each(function() {
-    const self = d3.select(this);
-    self.style('display', 'grid');
-    self.style('display', '-ms-grid');
-    self.style('grid-template-columns', 'minmax(0em,max-content) 0fr 1fr 0fr minmax(0em,max-content)');
-    self.style('-ms-grid-columns', 'minmax(0em,max-content) 0fr 1fr 0fr minmax(0em,max-content)');
-    self.style('grid-template-rows', 'minmax(0em,max-content) 0fr 1fr 0fr minmax(0em,max-content)');
-    self.style('-ms-grid-rows', 'minmax(0em,max-content) 0fr 1fr 0fr minmax(0em,max-content)');
-  });
-}
-
-function flatgrid(sel) {
-  return sel.each(function() {
-    const self = d3.select(this);
-    self.style('grid-template-rows', 'minmax(0em, max-content) 0fr 0fr 0fr minmax(0em, max-content)');
-    self.style('-ms-grid-rows', 'minmax(0em, 0em) 0fr 0fr 0fr minmax(0em, 0em)');
-  });
-}
-
-function flex(f) {
-  return function(sel) {
-    return sel.each(function() {
-      const self = d3.select(this);
-      self.style("flex", f + "");
-    });
-  };
-}
-
-function fontsize(fs) {
-  return function(sel) {
-    return sel.each(function() {
-      const self = d3.select(this);
-      self.style("font-size", fs + "px");
-    });
-  };
-}
-
-function borderBottom(bt) {
-  return function(sel) {
-    return sel.each(function() {
-      const self = d3.select(this);
-      self.style("border-bottom", bt);
-    });
-  };
-}
-
-
-const ohlcChart = fc
-  .chartCartesian(xScale, yScale)
-  .webglPlotArea(candlestick)
-  .svgPlotArea(lowLine)
-  .decorate(sel => {
-    sel.enter().call(nogrid);
-    sel.enter()
-      .select('.svg-plot-area')
-      .call(borderBottom('1px solid rgba(0, 0, 0, 0.1)'));
-    sel.enter()
-      .selectAll('.plot-area')
-      .call(zoom, xScale, yScale)
-    sel.enter()
-      .selectAll('.x-axis')
-      .call(hide, zoom, xScale);
-    sel.enter()
-      .selectAll('.top-label')
-      .call(hide);
-  });
-
-const volumeChart = fc
-  .chartCartesian(xScale, volumeScale)
-  .webglPlotArea(volume)
-  .svgPlotArea(lowLine)
-  .decorate(sel => {
-    sel.enter().call(nogrid);
-    sel.enter()
-      .selectAll('.plot-area')
-      .call(zoom, xScale, yScale)
-    sel.enter()
-      .selectAll('.x-axis')
-      .call(zoom, xScale);
-    sel.enter()
-      .selectAll('.top-label')
-      .call(hide);
-    sel.enter()
-      .selectAll('.right-axis svg')
-      .call(hide);
-    sel.enter()
-      .selectAll('svg')
-      .call(fontsize(14));
-  });
-
-const notInBounds = ({x, y}) => {
-  return x < 0 || y < 0 || x > xScale.range()[1] || y > yScale.range()[0];
-};
-
-function render() {
-  d3.select('#ohlc-chart')
-    .datum(data)
-    .call(ohlcChart);
-
-  d3.select('#volume-chart')
-    .datum(data)
-    .call(volumeChart);
-
-  if (!state.volumeVisible) {
-    d3.selectAll('#volume-chart .plot-area').call(hide);
-    d3.selectAll('#volume-chart .cartesian-chart').call(flatgrid);
-    d3.selectAll('#volume-chart').call(flex(0));
-  } else {
-    d3.selectAll('#volume-chart .plot-area').call(show);
-    d3.selectAll('#volume-chart .cartesian-chart').call(nogrid);
-    d3.selectAll('#volume-chart').call(flex(1));
-  }
-
-  renderCrosshair();
-}
-
-function getPriceAtPoint(y) {
-  return round2(yScale.invert(y));
-}
-
-function renderCrosshair() {
-  d3.select('#ohlc-chart svg')
-    .datum([mousePos])
-    .call(crosshair);
-
-  d3.select('#volume-chart svg')
-    .datum([mousePos])
-    .call(crosshairVertical);
-
-  const xLabelText = state.currentBar != null ? state.currentBar.date.toLocaleString() : "";
-  const xLabel = d3.select("#x-label");
-
-  const yLabelText = getPriceAtPoint(mousePos.y);
-  const yLabel = d3.select("#y-label");
-
-  if (notInBounds(mousePos)) {
-    xLabel.remove();
-    yLabel.remove();
-    return;
-  }
-
-  if (xLabel.empty()) {
-    d3.select('#volume-chart .bottom-axis svg')
-      .each(function() {
-        const self = d3.select(this);
-        const g = self.append('g')
-                      .attr('id', 'x-label')
-                      .attr('transform', `translate(${mousePos.x},16)`);
-
-        const rect1 = g.append('rect')
-                       .attr('id', 'rect1')
-                       .style('fill', 'white');
-
-        const rect2 = g.append('rect')
-                       .attr('id', 'rect2')
-                       .style('fill', 'black');
-
-        const text = g.append('text')
-                      .text(xLabelText)
-                      .style('fill', 'white');
-
-        const textNode = text.node();
-        const bbox = textNode.getBBox();
-
-        rect1.attr('x', bbox.x - 5)
-             .attr('y', bbox.y - 5)
-             .attr('width', bbox.width + 10)
-             .attr('height', bbox.height + 10);
-
-        rect2.attr('x', bbox.x - 5)
-             .attr('y', bbox.y - 5)
-             .attr('width', bbox.width + 10)
-             .attr('height', bbox.height + 10);
-      });
-  } else {
-    xLabel.each(function() {
-      const g = d3.select(this);
-      g.attr('transform', `translate(${mousePos.x},16)`);
-      const rect1 = g.select('rect#rect1').style('fill', 'white');
-      const rect2 = g.select('rect#rect2').style('fill', 'black');
-
-      const text = g.select('text')
-                    .text(xLabelText);
-
-      const textNode = text.node();
-      const bbox = textNode.getBBox();
-
-      rect1.attr('x', bbox.x - 5)
-           .attr('y', bbox.y - 5)
-           .attr('width', bbox.width + 10)
-           .attr('height', bbox.height + 10);
-
-      rect2.attr('x', bbox.x - 5)
-           .attr('y', bbox.y - 5)
-           .attr('width', bbox.width + 10)
-           .attr('height', bbox.height + 10);
-
-      g.raise();
-    });
-  }
-
-  if (yLabel.empty()) {
-    d3.select('#ohlc-chart .right-axis svg')
-      .each(function() {
-        const self = d3.select(this);
-        const g = self.append('g')
-                      .attr('id', 'y-label')
-                      .attr('transform', `translate(0,${mousePos.y})`);
-
-        const rect1 = g.append('rect')
-                       .attr('id', 'rect1')
-                       .style('fill', 'white');
-
-        const rect2 = g.append('rect')
-                       .attr('id', 'rect2')
-                       .style('fill', 'black');
-
-        const text = g.append('text')
-                      .text(yLabelText)
-                      .style('fill', 'white');
-
-        const textNode = text.node();
-        const bbox = textNode.getBBox();
-
-        rect1.attr('x', bbox.x - 5)
-             .attr('y', bbox.y - 5)
-             .attr('width', bbox.width + 10)
-             .attr('height', bbox.height + 10);
-
-        rect2.attr('x', bbox.x - 5)
-             .attr('y', bbox.y - 5)
-             .attr('width', bbox.width + 10)
-             .attr('height', bbox.height + 10);
-      });
-  } else {
-    yLabel.each(function() {
-      const g = d3.select(this);
-      g.attr('transform', `translate(0,${mousePos.y})`);
-      const rect1 = g.select('rect#rect1').style('fill', 'white');
-      const rect2 = g.select('rect#rect2').style('fill', 'black');
-
-      const text = g.select('text')
-                    .text(yLabelText);
-
-      const textNode = text.node();
-      const bbox = textNode.getBBox();
-
-      rect1.attr('x', bbox.x - 5)
-           .attr('y', bbox.y - 5)
-           .attr('width', bbox.width + 10)
-           .attr('height', bbox.height + 10);
-
-      rect2.attr('x', bbox.x - 5)
-           .attr('y', bbox.y - 5)
-           .attr('width', bbox.width + 10)
-           .attr('height', bbox.height + 10);
-
-      g.raise();
-    });
-  }
-}
-
-function getNearestOHLC(x, y) {
-  const xValue = xScale.invert(x);
-  const nearest = data.reduce((prev, curr) => {
-    return (Math.abs(curr.date - xValue) < Math.abs(prev.date - xValue) ? curr : prev);
-  });
-  const openY = yScale(nearest.open);
-  const highY = yScale(nearest.high);
-  const lowY = yScale(nearest.low);
-  const closeY = yScale(nearest.close);
-
-  const ohlcValues = [nearest.open, nearest.high, nearest.low, nearest.close];
-  const nearestValue = ohlcValues.reduce((prevVal, currVal) => {
-    const prev = yScale(prevVal);
-    const curr = yScale(currVal);
-    return (Math.abs(curr - mousePos.y) < Math.abs(prev - mousePos.y) ? currVal : prevVal);
-  });
-
-  return {
-    date: nearest.date,
-    value: nearestValue,
-  };
-}
-
-function snapMouseToOHLC(nearest) {
-  // Convert OHLC values to y-coordinates
-  const openY = yScale(nearest.open);
-  const highY = yScale(nearest.high);
-  const lowY = yScale(nearest.low);
-  const closeY = yScale(nearest.close);
-
-  // Array of OHLC y-coordinates
-  const ohlcValues = [openY, highY, lowY, closeY];
-
-  // Find the nearest OHLC value to the current mouse Y position
-  const nearestValue = ohlcValues.reduce((prev, curr) => {
-    return (Math.abs(curr - mousePos.y) < Math.abs(prev - mousePos.y) ? curr : prev);
-  });
-
-  // Set mousePos.x to the x-coordinate of the OHLC bar
-  mousePos.x = xScale(nearest.date);
-
-  // Set mousePos.y to the nearest OHLC value's y-coordinate
-  mousePos.y = nearestValue;
-}
-
 let shiftKeyPressed = false;
 
 document.addEventListener('keydown', (e) => {
@@ -540,6 +305,23 @@ document.addEventListener('keyup', (e) => {
   }
 });
 
+function snapMouseToOHLC(nearest) {
+  const openY = yScale(nearest.open);
+  const highY = yScale(nearest.high);
+  const lowY = yScale(nearest.low);
+  const closeY = yScale(nearest.close);
+
+  const ohlcValues = [openY, highY, lowY, closeY];
+
+  // Find the nearest OHLC value to the current mouse Y position
+  const nearestValue = ohlcValues.reduce((prev, curr) => {
+    return (Math.abs(curr - mousePos.y) < Math.abs(prev - mousePos.y) ? curr : prev);
+  });
+
+  mousePos.x = xScale(nearest.date);
+  mousePos.y = nearestValue;
+}
+
 function updateCrosshair() {
   const xValue = xScale.invert(mousePos.x);
   const nearest = data.reduce((prev, curr) => {
@@ -553,6 +335,98 @@ function updateCrosshair() {
   updateCurrentPrices(nearest);
 
   renderCrosshair();
+}
+
+const notInBounds = ({ x, y }) => {
+  return x < 0 || y < 0 || x > xScale.range()[1] || y > yScale.range()[0];
+};
+
+function renderCrosshair() {
+  d3.select('#ohlc-chart svg')
+    .datum([mousePos])
+    .call(crosshair);
+
+  d3.select('#volume-chart svg')
+    .datum([mousePos])
+    .call(crosshairVertical);
+
+  // Some complicated code follows to make the x and y axis labels work smoothly
+
+  const xLabelText = state.currentBar != null ? state.currentBar.date.toLocaleString() : "";
+  const xLabel = d3.select("#x-label");
+
+  const yLabelText = round2(yScale.invert(mousePos.y));
+  const yLabel = d3.select("#y-label");
+
+  if (notInBounds(mousePos)) {
+    xLabel.remove();
+    yLabel.remove();
+    return;
+  }
+
+  const adjustLabel = (g, rect1, rect2, text) => {
+    const label = g.attr('id');
+    const transform = label === 'x-label' ? `translate(${mousePos.x},16)` : `translate(0,${mousePos.y})`;
+    g.attr('transform', transform);
+    rect1.style('fill', 'white');
+    rect2.style('fill', 'black');
+    text.text(label === 'x-label' ? xLabelText : yLabelText).style('fill', 'white');
+
+    const textNode = text.node();
+    const bbox = textNode.getBBox();
+
+    rect1.attr('x', bbox.x - 5)
+      .attr('y', bbox.y - 5)
+      .attr('width', bbox.width + 10)
+      .attr('height', bbox.height + 10);
+
+    rect2.attr('x', bbox.x - 5)
+      .attr('y', bbox.y - 5)
+      .attr('width', bbox.width + 10)
+      .attr('height', bbox.height + 10);
+
+    g.raise();
+  };
+
+  if (xLabel.empty()) {
+    d3.select('#volume-chart .bottom-axis svg')
+      .each(function () {
+        const self = d3.select(this);
+        const g = self.append('g').attr('id', 'x-label');
+        const rect1 = g.append('rect').attr('id', 'rect1');
+        const rect2 = g.append('rect').attr('id', 'rect2');
+        const text = g.append('text');
+        adjustLabel(g, rect1, rect2, text);
+      });
+  } else {
+    xLabel.each(function () {
+      const g = d3.select(this);
+      const rect1 = g.select('rect#rect1');
+      const rect2 = g.select('rect#rect2');
+      const text = g.select('text');
+      adjustLabel(g, rect1, rect2, text);
+    });
+  }
+
+  if (yLabel.empty()) {
+    d3.select('#ohlc-chart .right-axis svg')
+      .each(function () {
+        const self = d3.select(this);
+        const g = self.append('g').attr('id', 'y-label');
+        const rect1 = g.append('rect').attr('id', 'rect1');
+        const rect2 = g.append('rect').attr('id', 'rect2');
+        const text = g.append('text');
+        adjustLabel(g, rect1, rect2, text);
+      });
+  } else {
+    yLabel.each(function () {
+      const g = d3.select(this);
+      const rect1 = g.select('rect#rect1');
+      const rect2 = g.select('rect#rect2');
+      const text = g.select('text');
+      adjustLabel(g, rect1, rect2, text);
+    });
+  }
 }
 
 d3.select('#ohlc-chart')
@@ -572,5 +446,29 @@ d3.select('#ohlc-chart')
 
     updateCrosshair();
   });
+
+// To close it out, define the charts and render them
+
+function render() {
+  d3.select('#ohlc-chart')
+    .datum(data)
+    .call(ohlcChart);
+
+  d3.select('#volume-chart')
+    .datum(data)
+    .call(volumeChart);
+
+  if (!state.volumeVisible) {
+    d3.selectAll('#volume-chart .plot-area').call(displayNone);
+    d3.selectAll('#volume-chart .cartesian-chart').call(flatgrid);
+    d3.selectAll('#volume-chart').call(attr("flex", "0"));
+  } else {
+    d3.selectAll('#volume-chart .plot-area').call(attr('display', 'block'));
+    d3.selectAll('#volume-chart .cartesian-chart').call(specialgrid);
+    d3.selectAll('#volume-chart').call(attr("flex", "1"));
+  }
+
+  renderCrosshair();
+}
 
 render();
